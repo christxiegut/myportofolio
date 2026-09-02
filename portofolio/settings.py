9925c2ac4f8d96b5dc44b4156ee13e245fcae495
christxiegut <angeliciperkici@gmail.com>
2026-09-02 19:28:16 +0700

    Trigger rebuild PWS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portofolio.wsgi.application'
 
-# Database
+# Database 
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 # Database configuration
